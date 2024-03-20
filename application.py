0286291f17508bd8233c628b44c78cedc93df160
RahulM-3 <rahulcode97@gmail.com>
2024-03-21 03:27:10 +0530

    check box using direct html

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from comparison import comparison
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+import streamlit.components.v1 as components
+st.set_page_config(layout="wide")
 
 csvfile = st.file_uploader(type="csv", label="upload csv file", accept_multiple_files=False)
 if(csvfile != None):
@@ -12,25 +14,32 @@ if(csvfile != None):
                         "Random Forest Regression"]
     dataframe = pd.read_csv(csvfile)
 
-    section1 = st.columns(2)
+    section1 = st.columns([20, 20])
     with section1[0]:
         st.write(dataframe)
     
     with section1[1]:
-        checkboxes = st.columns(2)
+        checkboxes = st.columns([10, 10])
+        indeside = "<div id='indeside'>"
+        deside = "<div id='deside'>"
+        
+        with checkboxes[0]:
+            st.write("Independent Variable")
+            for label in dataframe.columns:
+                indeside += f'''<input type="checkbox" id="{label}in"> <label for={label}in>{label}</label> <br>'''
+            st.markdown(indeside, unsafe_allow_html=True)
+        
+        with checkboxes[1]:
+            st.write("Dependent Variable")
+            for label in dataframe.columns:
+                deside += f'''<input type="checkbox" id="{label}de"> <label for={label}de>{label}</label> <br>'''
+            st.markdown(deside, unsafe_allow_html=True)
 
-        st.write("Select role of the attribute or drop the attribute")
-        dependent = dataframe.columns[-1]
-        independent = dataframe.columns[:-1]
-        trackradio = {}
+        javascript = '<script>parent.document.getElementsByTagName("iframe")[0].hidden="hidden";</script>'
+        components.html(javascript)
 
-        for label in dataframe.columns:
-            trackradio[label] = st.radio(label=label+":", options=["Independent", "Dependent", "Drop"], horizontal=True)
-            if(label == dependent):
-                trackradio[label] = st.radio(label=label+":", options=["Independent", "Dependent", "Drop"], index=1, horizontal=True)
-
-    model = comparison(dataframe, dependent=dependent, independent=independent)
-    st.write(model.RegressionModels(availablemodel))
+    #model = comparison(dataframe, dependent=dependent, independent=independent)
+    #st.write(model.RegressionModels(availablemodel))
 
     
 
