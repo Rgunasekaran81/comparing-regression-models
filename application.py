b47509725ce1eae9bdf782835d1f1d6cd40e599f
RahulM-3 <rahulcode97@gmail.com>
2024-03-20 22:48:10 +0530

    function to predict user data

--- application.py
+++ application.py
@@ -6,8 +6,14 @@ csvfile = st.file_uploader(type="csv", label="upload csv file", accept_multiple_
 if(csvfile != None):
     dataframe = pd.read_csv(csvfile)
     st.write(dataframe)
-    model = comparison(dataframe, "Sales")
-    st.write(model.LassoReg())
+    availablemodel = ["Ridge Regression", 
+                        "Linear Regression",
+                        "Lasso Regression",
+                        "Xgboost Regression",
+                        "Random Forest Regression"]
+    
+    model = comparison(dataframe, dataframe.columns[-1])
+    st.write(model.RegressionModels(availablemodel))
 
     
 
--- comparison.py
+++ comparison.py
@@ -17,7 +17,7 @@ class comparison:
 
     def __init__(self, dataframe: pd, dependent:str, dropattribute:list[str]=[], test_size=0.2, cvepoch:int=None, lassoalpha:float=0.1) -> None:
         self.dataframe = dataframe.drop(dropattribute, axis=1)
-        self.dependent  = dependent 
+        self.dependent  = dependent
         self.cvepoch = cvepoch
 
         self.ridge_model = Ridge(True)
@@ -25,57 +25,64 @@ class comparison:
         self.lasso_model = Lasso(alpha = lassoalpha)
         self.xgboost_model = XGBRegressor()
         self.randomforest_reg = RandomForestRegressor()
+        self.modelist = {"Ridge Regression":self.ridge_model, 
+                        "Linear Regression":self.linear_model,
+                        "Lasso Regression":self.lasso_model,
+                        "Xgboost Regression":self.xgboost_model,
+                        "Random Forest Regression":self.randomforest_reg
+                        }
+
+        #self.datafromat = self.cleandata()
 
         X = self.dataframe.drop(self.dependent, axis=1)
         y = self.dataframe[self.dependent]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=1)
 
-    def eval_metrices(self, actual, pred):
+    def eval_metrices(self, actual, pred) -> list[float]:
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         mse = mean_squared_error(actual, pred)
         score = r2_score(actual, pred)
         return [rmse, mae, mse, score]
 
-    def crossvalidation(self, model_name, X_train, y_train):
+    def crossvalidation(self, model_name, X_train, y_train) -> list[float]:
         cv_score = cross_val_score(estimator=model_name, X = X_train, y = y_train, cv=self.cvepoch)
         return cv_score.mean()
 
-    def RidgeReg(self):
-        self.ridge_model.fit(self.X_train, self.y_train)
-        y_pred = self.ridge_model.predict(self.X_test)
+    def cleandata(self) -> dict[str:dict[str:int]]:
+        def value_map(x, valuemap) -> int:
+            for key, value in valuemap.items():
+                if(x == key):
+                    return value
+        changein = {}
+        for col in self.dataframe:
+            values = self.dataframe[col].unique()
+            if(type(values[0]) == str):
+                valuemap = {}
+                for num, val in enumerate(values):
+                    valuemap[val] = num
+                changein[col] = valuemap
+                self.dataframe[col] = valuemap
+                self.dataframe[col] = self.dataframe[col].apply(value_map, valuemap=valuemap)
+        return changein
 
-        cval_score = self.crossvalidation(self.ridge_model, self.X_train, self.y_train)
-        return [self.ridge_model, self.eval_metrices(self.y_test, y_pred), cval_score]
+    def RegressionModels(self, usermodellist:list[str]) -> dict[str:list]:
+        modeldata = {}
+        for model in usermodellist:
+            curmodel = self.modelist[model]
+            curmodel.fit(self.X_train, self.y_train)
+            y_pred = self.ridge_model.predict(self.X_test)
 
-    def LinearReg(self):
+            cval_score = self.crossvalidation(curmodel, self.X_train, self.y_train)
+            modeldata[model] = [curmodel, self.eval_metrices(self.y_test, y_pred), cval_score]
         
-        self.linear_model.fit(self.X_train, self.y_train)
-        y_pred = self.linear_model.predict(self.X_test)
+        return modeldata
 
-        cval_score = self.crossvalidation(self.linear_model, self.X_train, self.y_train)
-        return [self.ridge_model, self.eval_metrices(self.y_test, y_pred), cval_score]
-
-    def LassoReg(self):
-        self.lasso_model.fit(self.X_train, self.y_train)
-        y_pred = self.lasso_model.predict(self.X_test)
+    def predict_y(self, data, usermodelist) -> dict[str:list[float]]:
+        modelprediction = {}
+        for model in usermodelist:
+            prediction = self.modelist[model].predict(data)
+            confidence = round(100 * (np.max(prediction[0])), 2)
+            modelprediction[model] = [prediction, confidence]
         
-        cval_score = self.crossvalidation(self.lasso_model, self.X_train, self.y_train)
-        return [self.ridge_model, self.eval_metrices(self.y_test, y_pred), cval_score]
-    
-    def XgboostReg(self):
-        self.xgboost_model.fit(self.X_train, self.y_train)
-        y_pred = self.xgboost_model.predict(self.X_test)
-        
-        cval_score = self.crossvalidation(self.xgboost_model, self.X_train, self.y_train)
-        return [self.ridge_model, self.eval_metrices(self.y_test, y_pred), cval_score]
-
-    def RandomForestReg(self):
-        self.randomforest_reg.fit(self.X_train, self.y_train)
-        y_pred = self.randomforest_reg.predict(self.X_test)
-
-        cval_score = self.crossvalidation(self.randomforest_reg, self.X_train, self.y_train)
-        return [self.randomforest_reg, self.eval_metrices(self.y_test, y_pred), cval_score]
-
-
-
+        return modelprediction
