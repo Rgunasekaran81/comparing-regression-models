7177fc913976f76164785d33501932da42b72553
RahulM-3 <rahulcode97@gmail.com>
2024-03-21 01:40:19 +0530

    pick dependent and independent or drop a attribute

--- application.py
+++ application.py
@@ -4,32 +4,33 @@ import pandas as pd
 
 csvfile = st.file_uploader(type="csv", label="upload csv file", accept_multiple_files=False)
 if(csvfile != None):
-    dataframe = pd.read_csv(csvfile)
-    st.write(dataframe)
+
     availablemodel = ["Ridge Regression", 
                         "Linear Regression",
                         "Lasso Regression",
                         "Xgboost Regression",
                         "Random Forest Regression"]
-    checkboxes = st.columns(2)
-    trackcheckbox = {} 
-    with checkboxes[0]:
-        st.write("select dependent attributes")
-    for label in dataframe.columns:
-        with checkboxes[0]:
-            trackcheckbox[label+"0"] = st.checkbox(label=label, key=label+"0")
-             
-    with checkboxes[1]:
-        st.write("select independent attributes")
-    for label in dataframe.columns:
-        with checkboxes[1]:
-            trackcheckbox[label+"1"] = st.checkbox(label=label, key=label+"1")
+    dataframe = pd.read_csv(csvfile)
 
-    attributeselected = False
-    dependent = []
-    independent = None
-   # model = comparison(dataframe, dataframe.columns[-1])
-    #st.write(model.RegressionModels(availablemodel))
+    section1 = st.columns(2)
+    with section1[0]:
+        st.write(dataframe)
+    
+    with section1[1]:
+        checkboxes = st.columns(2)
+
+        st.write("Select role of the attribute or drop the attribute")
+        dependent = dataframe.columns[-1]
+        independent = dataframe.columns[:-1]
+        trackradio = {}
+
+        for label in dataframe.columns:
+            trackradio[label] = st.radio(label=label+":", options=["Independent", "Dependent", "Drop"], horizontal=True)
+            if(label == dependent):
+                trackradio[label] = st.radio(label=label+":", options=["Independent", "Dependent", "Drop"], index=1, horizontal=True)
+
+    model = comparison(dataframe, dependent=dependent, independent=independent)
+    st.write(model.RegressionModels(availablemodel))
 
     
 
