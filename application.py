d73a4282e9c9ba933374400a2ea261270ec0353a
RahulM-3 <rahulcode97@gmail.com>
2024-03-21 09:33:36 +0530

    can only select a variable in one role i.e dependent or independent

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import pandas as pd
 import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 
+print("test")
 csvfile = st.file_uploader(type="csv", label="upload csv file", accept_multiple_files=False)
 if(csvfile != None):
 
@@ -26,16 +27,50 @@ if(csvfile != None):
         with checkboxes[0]:
             st.write("Independent Variable")
             for label in dataframe.columns:
-                indeside += f'''<input type="checkbox" id="{label}in"> <label for={label}in>{label}</label> <br>'''
+                id = label.replace(" ","")+"in"
+                indeside += f'''<input type="checkbox" id="{id}"> <label for={id}>{label}</label> <br>'''
             st.markdown(indeside, unsafe_allow_html=True)
         
         with checkboxes[1]:
             st.write("Dependent Variable")
             for label in dataframe.columns:
-                deside += f'''<input type="checkbox" id="{label}de"> <label for={label}de>{label}</label> <br>'''
+                id = label.replace(" ","")+"de"
+                deside += f'''<input type="checkbox" id="{id}"> <label for={id}>{label}</label> <br>'''
             st.markdown(deside, unsafe_allow_html=True)
 
-        javascript = '<script>parent.document.getElementsByTagName("iframe")[0].hidden="hidden";</script>'
+        code = ""            
+        for label in dataframe.columns:
+            id1 = label.replace(" ","")
+            if(label != dataframe.columns[-1]):
+                code += '''
+                    parent.document.getElementById('''+'"'+id1+'in'+'"'+''').checked = true;
+                    parent.document.getElementById('''+'"'+id1+'de'+'"'+''').disabled = true;
+                        '''
+            else:
+                code += '''
+                    parent.document.getElementById('''+'"'+id1+'de'+'"'+''').checked = true;
+                    parent.document.getElementById('''+'"'+id1+'in'+'"'+''').disabled = true;
+                        '''
+            code += '''
+                checkboxin = parent.document.getElementById('''+'"'+id1+'in'+'"'+''');
+                checkboxin.addEventListener('change', function() {
+                    if (this.checked) {
+                        parent.document.getElementById('''+'"'+id1+'de'+'"'+''').disabled = true;
+                    } else {
+                        parent.document.getElementById('''+'"'+id1+'de'+'"'+''').disabled = false;
+                    }
+                });
+                checkboxde = parent.document.getElementById('''+'"'+id1+'de'+'"'+''');
+                checkboxde.addEventListener('change', function() {
+                    console.log(this.checked)
+                    if (this.checked) {
+                        parent.document.getElementById('''+'"'+id1+'in'+'"'+''').disabled = true;
+                    } else {
+                        parent.document.getElementById('''+'"'+id1+'in'+'"'+''').disabled = false;
+                    }
+                });
+                    '''
+        javascript = f'<script>parent.document.getElementsByTagName("iframe")[0].hidden="hidden";{code}</script>'
         components.html(javascript)
 
     #model = comparison(dataframe, dependent=dependent, independent=independent)
